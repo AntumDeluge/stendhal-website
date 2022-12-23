216f0d27b3003ee487bc09ab9d39ba7f5d612ffe
Jordan Irwin <antumdeluge@gmail.com>
2022-12-23 06:55:57 -0800

    Calculate 'Maze Runner' rank from maze time on character page

--- content/scripts/character.php
+++ content/scripts/character.php
@@ -151,7 +151,7 @@ foreach($choosen->equipment as $slot=>$content) {
 		echo htmlspecialchars($choosen->name). ' is <br>new in Stendhal.<br><br>Please check back<br> one day after reaching <br>level 2 because <br>ranks are only <br>calculated once a day.';
 	} else {
 		$names = array('Best', 'XP', 'Achievements', 'Richest', 'Eldest', 'Deathmatch', 'Attackers', 'Defenders', 'Maze Runner');
-		$fametypes = array('R', 'X', '@', 'W', 'A', 'D', 'T', 'F', 'M');
+		$fametypes = array('R', 'X', '@', 'W', 'A', 'D', 'T', 'F', 'MT');
 		for ($i = 0; $i < count($names); $i++) {
 			echo '<div><span class="statslabel">'.$names[$i].'</span><span class="data">';
 			if (isset($ranks[$fametypes[$i]])) {
