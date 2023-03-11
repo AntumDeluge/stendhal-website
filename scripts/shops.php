8e137c738134aafe38abf83c154b9f72b530534d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2023-03-11 22:51:14 +0100

    moved shop handling from npcs.php to new file shops.php

---
+++ scripts/shops.php
@@ -0,0 +1,229 @@
+<?php
+/*
+ Stendhal website - a website to manage and ease playing of Stendhal game
+ Copyright (C) 2009-2023 Stendhal
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+
+// shops lists
+$npcshops;
+
+
+function parseAddRequire($reqdata) {
+	$required;
+	if (!is_array($reqdata)) {
+		return;
+	}
+	for ($idx = 0; $idx < sizeof($reqdata); $idx++) {
+		if (!isset($reqdata[$idx." attr"])) {
+			continue;
+		}
+		$tmp = $reqdata[$idx." attr"];
+		if (isset($tmp["name"]) && isset($tmp["count"])) {
+			$name = $tmp["name"];
+			$item = getItem($name);
+			if ($item != null) {
+				$name = $item->createNameLink();
+			}
+			if (!isset($required)) {
+				$required = $tmp["count"]." ".$name;
+			} else {
+				$required .= " + ".$tmp["count"]." ".$name;
+			}
+		}
+	}
+	return $required;
+}
+
+
+/**
+ * Parses shop info from xml data.
+ *
+ * @param shopsdata
+ *     XML data containing shops information.
+ */
+function parseShopsData($shopsdata) {
+	global $npcshops;
+
+	if (!isset($npcshops)) {
+		$npcshops = [];
+	}
+
+	if (sizeof($shopsdata) < 2) {
+		return;
+	}
+
+	for ($idx = 0; $idx < sizeof($shopsdata) / 2; $idx++) {
+		if (!isset($shopsdata[$idx]["merchant"])) {
+			continue;
+		}
+
+		$attr = [];
+		if (isset($shopsdata[$idx." attr"])) {
+			$attr = $shopsdata[$idx." attr"];
+		}
+		if (!isset($attr["type"])) {
+			continue;
+		}
+		$shoptype = $attr["type"];
+		if ($shoptype === "trade") {
+			$shoptype = "sell";
+		}
+
+		$merchants = [];
+		$tmp = $shopsdata[$idx]["merchant"];
+		for ($m = 0; $m < sizeof($tmp) / 2; $m++) {
+			if (!isset($tmp[$m." attr"])) {
+				continue;
+			}
+			$merchant = $tmp[$m." attr"];
+			if (isset($merchant["name"])) {
+				$merchants[$merchant["name"]] = isset($merchant["note"]) ? $merchant["note"] : null;
+			}
+		}
+
+		$itemlist = [];
+		$contents = null;
+		if ($shoptype === "outfit") {
+			if (isset($shopsdata[$idx]["outfit"])) {
+				$contents = $shopsdata[$idx]["outfit"];
+			}
+		} else {
+			if (isset($shopsdata[$idx]["item"])) {
+				$contents = $shopsdata[$idx]["item"];
+			}
+		}
+		if (isset($contents)) {
+			for ($i = 0; $i < sizeof($contents) / 2; $i++) {
+				if (!isset($contents[$i." attr"])) {
+					continue;
+				}
+
+				$item = $contents[$i." attr"];
+				$requirealso = null;
+				if (isset($item["name"])) {
+					if (isset($contents[$i]["for"])) {
+						$requirealso = parseAddRequire($contents[$i]["for"]);
+					}
+
+					$price = isset($item["price"]) ? $item["price"] : "";
+					if (strlen($price) > 0 && isset($item["pricemax"])) {
+						$price .= "-".$item["pricemax"];
+					}
+					$price = strlen($price) > 0 ? $price." money" : $price;
+					if (isset($requirealso)) {
+						if (strlen($price) > 0) {
+							$price .= " + ";
+						}
+						$price .= $requirealso;
+					}
+					$itemname = $item["name"];
+					$itemlist[$itemname]["price"] = $price;
+					if ($shoptype === "outfit" && isset($item["layers"])) {
+						$itemlist[$itemname]["layers"] = formatOutfit($item["layers"]);
+					}
+					if (isset($item["note"])) {
+						$itemlist[$itemname]["note"] = $item["note"];
+					}
+				}
+			}
+		}
+
+		foreach ($merchants as $npcname=>$shopnote) {
+			// allow merchants configured for multiple shops
+			if (isset($npcshops[$npcname][$shoptype])) {
+				$itemlist = array_merge($npcshops[$npcname][$shoptype], $itemlist);
+			}
+			if (isset($shopnote)) {
+				$itemlist["__shopnote__"] = $shopnote;
+			}
+			$npcshops[$npcname][$shoptype] = $itemlist;
+		}
+	}
+}
+
+
+/**
+ * Loads registered shops from config.
+ */
+function loadShops() {
+	global $cache;
+	global $npcshops;
+
+	if (!isset(NPC::$shops) || sizeof(NPC::$shops) == 0) {
+		NPC::$shops = $cache->fetchAsArray("stendhal_shops");
+	}
+	if (is_array(NPC::$shops) && sizeof(NPC::$shops) > 0) {
+		return;
+	}
+
+
+	$content = file("data/conf/shops.xml");
+	$tmp = implode("", $content);
+	$groot = XML_unserialize($tmp);
+
+	if (!isset($groot["groups"][0]["group"])) {
+		return;
+	}
+
+	$tmp = $groot["groups"][0]["group"];
+	$groups = [];
+	for ($idx = 0; $idx < sizeof($tmp) / 2; $idx++) {
+		if (isset($tmp[$idx." attr"]) && isset($tmp[$idx." attr"]["uri"])) {
+			$groups[] = "data/conf/".$tmp[$idx." attr"]["uri"];
+		}
+	}
+	foreach ($groups as $group) {
+		$content = file($group);
+		$tmp = implode("", $content);
+		$root = XML_unserialize($tmp);
+		if (!isset($root["shops"][0]["shop"])) {
+			continue;
+		}
+		parseShopsData($root["shops"][0]["shop"]);
+	}
+
+	//~ NPC::$shops = $npcshops;
+	//~ $cache->store("stendhal_shops", new ArrayObject($npcshops));
+}
+
+/**
+ * Retrieves shop lists for an NPC.
+ *
+ * @param npcname
+ *     Name of NPC for which shop is associated.
+ * @return
+ *     "sell" &/or "buy" shop lists or <code>null</code> if the NPC
+ *     does not have a shop.
+ */
+function getNPCShop($npcname) {
+	global $npcshops;
+
+	loadShops();
+	if (isset($npcshops[$npcname])) {
+		return $npcshops[$npcname];
+	}
+}
+
+/**
+ * Retrieves all NPC shops.
+ */
+function getNPCShops() {
+	global $npcshops;
+
+	loadShops();
+	return $npcshops;
+}
