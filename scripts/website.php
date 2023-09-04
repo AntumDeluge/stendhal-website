4501a82d373203da63590cc97b4853e1ed509e37
Hendrik Brummermann <nhnb@users.sourceforge.net>
2023-09-04 08:31:43 +0200

    fixed hash directory calculation for wiki cache

--- scripts/website.php
+++ scripts/website.php
@@ -172,9 +172,10 @@ class Wiki {
 	private function get($page) {
 
 		// check file cache
+		$page = 'ns0:' . $page;
 		$md5 = md5($page);
 		$path = '/var/www/stendhal/w/images/cache/'.$md5[0].'/'.$md5[0].$md5[1]
-			.'/ns0%3A'.str_replace('/', '%2F', urlencode($page)).'.html';
+			.'/'.str_replace('/', '%2F', urlencode($page)).'.html';
 		$content = @file_get_contents($path);
 
 		if ($content === false) {
