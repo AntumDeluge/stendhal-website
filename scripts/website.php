e00348d16071777c868b3bfba3bd5038c0aceb35
Hendrik Brummermann <nhnb@users.sourceforge.net>
2023-09-04 08:50:53 +0200

    updated content extraction to match new Wiki html structure

--- scripts/website.php
+++ scripts/website.php
@@ -223,8 +223,8 @@ class Wiki {
 	 */
 	private function clean($content) {
 		// remove wiki navigation
-		$start = strpos($content, '<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">');
-		$end = strrpos($content, '<div class="printfooter">');
+		$start = strpos($content, '<div id="mw-content-text"');
+		$end = strrpos($content, '<div class="printfooter"');
 		$content = '<div id="mw-content-text">'.substr($content, $start, $end - $start);
 
 		// remove content which was marked as to be removed
